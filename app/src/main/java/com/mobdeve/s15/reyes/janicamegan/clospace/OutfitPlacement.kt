cd5f87b594f447a4c13437c5055f88a68284eccc
3clipseee <gabriel_benedict_esguerra@dlsu.edu.ph>
2026-08-12 17:21:42 +0800

    Clear all files

--- app/src/main/java/com/mobdeve/s15/reyes/janicamegan/clospace/OutfitPlacement.kt
+++
@@ -1,9 +0,0 @@
-package com.mobdeve.s15.reyes.janicamegan.clospace
-
-data class OutfitPlacement(
-    val item: ClothingItem,
-    val x: Float,
-    val y: Float,
-    val scale: Float,
-    val layer: Int
-)
--- app/src/main/java/com/mobdeve/s15/reyes/janicamegan/clospace/OutfitWithItems.kt
+++
@@ -1,6 +0,0 @@
-package com.mobdeve.s15.reyes.janicamegan.clospace
-
-data class OutfitWithItems(
-    val outfit: Outfit,
-    val placements: List<OutfitPlacement> = emptyList()
-)
